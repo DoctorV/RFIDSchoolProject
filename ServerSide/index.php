5ee818d0a385f26f002e126efb0f756895e5881f
octoleaf <bill.voisine@octoleaf.com>
2014-03-09 11:42:33 -0400

    modified:   ServerSide/index.php

--- ServerSide/index.php
+++ ServerSide/index.php
@@ -24,57 +24,56 @@ if (@ftp_login($conn_id, $ftp_user, $ftp_pass)) {
 	
 	$contents = ftp_nlist($conn_id, ".");
 	
-	/*foreach ($contents as $v) {
-		echo $v, "<BR>";
-	}*/
-	
 	foreach ($contents as $v) {
 		if ($v == "bed.txt") {
-			$x = 200;
-			$y = 500;
-			echo "<img STYLE='position:absolute; TOP:500px; LEFT:300px;' src='.\images\lightOn.png'>";
-			echo "<img STYLE='position:absolute; TOP:200px; RIGHT:600px;' src='.\images\lightOff.png'>";
-			echo "<img STYLE='position:absolute; TOP:50px; RIGHT:1050px;' src='.\images\lightOff.png'>";
-			echo "<img STYLE='position:absolute; TOP:700px; RIGHT:120px;' src='.\images\home.png'>";
-			echo "<img STYLE='position:absolute; TOP:800px; RIGHT:120px;' src='.\images\Green.png'>";
-			echo "<img STYLE='position:absolute; TOP:", $y, "px; LEFT:", $x, "px;' src='.\images\bill.png'>";
+			$x = 13;
+			$y = 65;
+			echo "<img STYLE='position:absolute; TOP:65%; LEFT:20%;' src='.\images\lightOn.png'>";
+			echo "<img STYLE='position:absolute; TOP:20%; RIGHT:30%;' src='.\images\lightOff.png'>";
+			echo "<img STYLE='position:absolute; TOP:65%; RIGHT:30%;' src='.\images\lightOff.png'>";
+			echo "<img STYLE='position:absolute; TOP:5%; LEFT:33%;' src='.\images\lightOff.png'>";
+			echo "<img STYLE='position:absolute; BOTTOM:30%; RIGHT:7%;' src='.\images\home.png'>";
+			echo "<img STYLE='position:absolute; BOTTOM:15%; RIGHT:7%;' src='.\images\Green.png'>";
+			echo "<img STYLE='position:absolute; TOP:", $y, "%; LEFT:", $x, "%;' src='.\images\bill.png'>";
 		} elseif ($v == "living.txt") {
-			$x = 700;
-			$y = 500;
-			echo "<img STYLE='position:absolute; TOP:500px; RIGHT:600px;' src='.\images\lightOn.png'>";
-			echo "<img STYLE='position:absolute; TOP:200px; RIGHT:600px;' src='.\images\lightOff.png'>";
-			echo "<img STYLE='position:absolute; TOP:50px; RIGHT:1050px;' src='.\images\lightOff.png'>";
-			echo "<img STYLE='position:absolute; TOP:700px; RIGHT:120px;' src='.\images\home.png'>";
-			echo "<img STYLE='position:absolute; TOP:500px; LEFT:300px;' src='.\images\lightOff.png'>";
-			echo "<img STYLE='position:absolute; TOP:800px; RIGHT:120px;' src='.\images\Green.png'>";
-			echo "<img STYLE='position:absolute; TOP:", $y, "px; RIGHT:", $x, "px;' src='.\images\bill.png'>";
+			$x = 35;
+			$y = 65;
+			echo "<img STYLE='position:absolute; TOP:65%; RIGHT:30%;' src='.\images\lightOn.png'>";
+			echo "<img STYLE='position:absolute; TOP:20%; RIGHT:30%;' src='.\images\lightOff.png'>";
+			echo "<img STYLE='position:absolute; TOP:65%; LEFT:20%;' src='.\images\lightOff.png'>";
+			echo "<img STYLE='position:absolute; TOP:5%; LEFT:33%;' src='.\images\lightOff.png'>";
+			echo "<img STYLE='position:absolute; BOTTOM:30%; RIGHT:7%;' src='.\images\home.png'>";
+			echo "<img STYLE='position:absolute; BOTTOM:15%; RIGHT:7%;' src='.\images\Green.png'>";
+			echo "<img STYLE='position:absolute; TOP:", $y, "%; RIGHT:", $x, "%;' src='.\images\bill.png'>";
 		} elseif ($v == "kitchen.txt") {
-			$x = 700;
-			$y = 200;
-			echo "<img STYLE='position:absolute; TOP:200px; RIGHT:600px;' src='.\images\lightOn.png'>";
-			echo "<img STYLE='position:absolute; TOP:700px; RIGHT:120px;' src='.\images\home.png'>";
-			echo "<img STYLE='position:absolute; TOP:500px; LEFT:300px;' src='.\images\lightOff.png'>";
-			echo "<img STYLE='position:absolute; TOP:50px; RIGHT:1050px;' src='.\images\lightOff.png'>";
-			echo "<img STYLE='position:absolute; TOP:800px; RIGHT:120px;' src='.\images\Green.png'>";
-			echo "<img STYLE='position:absolute; TOP:", $y, "px; RIGHT:", $x, "px;' src='.\images\bill.png'>";
+			$x = 35;
+			$y = 20;
+			echo "<img STYLE='position:absolute; TOP:20%; RIGHT:30%;' src='.\images\lightOn.png'>";
+			echo "<img STYLE='position:absolute; TOP:65%; RIGHT:30%;' src='.\images\lightOff.png'>";
+			echo "<img STYLE='position:absolute; TOP:65%; LEFT:20%;' src='.\images\lightOff.png'>";
+			echo "<img STYLE='position:absolute; TOP:5%; LEFT:33%;' src='.\images\lightOff.png'>";
+			echo "<img STYLE='position:absolute; BOTTOM:30%; RIGHT:7%;' src='.\images\home.png'>";
+			echo "<img STYLE='position:absolute; BOTTOM:15%; RIGHT:7%;' src='.\images\Green.png'>";
+			echo "<img STYLE='position:absolute; TOP:", $y, "%; RIGHT:", $x, "%;' src='.\images\bill.png'>";
 		} elseif ($v == "bath.txt") {
-			$x = 1100;
-			$y = 50;
-			echo "<img STYLE='position:absolute; TOP:700px; RIGHT:120px;' src='.\images\home.png'>";
-			echo "<img STYLE='position:absolute; TOP:50px; RIGHT:1050px;' src='.\images\lightOn.png'>";
-			echo "<img STYLE='position:absolute; TOP:500px; RIGHT:600px;' src='.\images\lightOff.png'>";
-			echo "<img STYLE='position:absolute; TOP:500px; LEFT:300px;' src='.\images\lightOff.png'>";
-			echo "<img STYLE='position:absolute; TOP:800px; RIGHT:120px;' src='.\images\Green.png'>";
-			echo "<img STYLE='position:absolute; TOP:", $y, "px; RIGHT:", $x, "px;' src='.\images\bill.png'>";	
+			$x = 27;
+			$y = 5;
+			echo "<img STYLE='position:absolute; TOP:5%; LEFT:33%;' src='.\images\lightOn.png'>";
+			echo "<img STYLE='position:absolute; TOP:20%; RIGHT:30%;' src='.\images\lightOff.png'>";
+			echo "<img STYLE='position:absolute; TOP:65%; RIGHT:30%;' src='.\images\lightOff.png'>";
+			echo "<img STYLE='position:absolute; TOP:65%; LEFT:20%;' src='.\images\lightOff.png'>";
+			echo "<img STYLE='position:absolute; BOTTOM:30%; RIGHT:7%;' src='.\images\home.png'>";
+			echo "<img STYLE='position:absolute; BOTTOM:15%; RIGHT:7%;' src='.\images\Green.png'>";
+			echo "<img STYLE='position:absolute; TOP:", $y, "%; LEFT:", $x, "%;' src='.\images\bill.png'>";	
 		} elseif ($v == "." or $v == ".." or $v == " ") {
 			$x = -100;
 			$y = -100;
-			echo "<img STYLE='position:absolute; TOP:200px; RIGHT:600px;' src='.\images\lightOff.png'>";
-			echo "<img STYLE='position:absolute; TOP:500px; RIGHT:600px;' src='.\images\lightOff.png'>";
-			echo "<img STYLE='position:absolute; TOP:500px; LEFT:300px;' src='.\images\lightOff.png'>";
-			echo "<img STYLE='position:absolute; TOP:50px; RIGHT:1050px;' src='.\images\lightOff.png'>";
-			echo "<img STYLE='position:absolute; TOP:700px; RIGHT:120px;' src='.\images\away.png'>";
-			echo "<img STYLE='position:absolute; TOP:800px; RIGHT:120px;' src='.\images\Red.png'>";
+			echo "<img STYLE='position:absolute; TOP:20%; RIGHT:30%;' src='.\images\lightOff.png'>";
+			echo "<img STYLE='position:absolute; TOP:65%; RIGHT:30%;' src='.\images\lightOff.png'>";
+			echo "<img STYLE='position:absolute; TOP:65%; LEFT:20%;' src='.\images\lightOff.png'>";
+			echo "<img STYLE='position:absolute; TOP:5%; LEFT:33%;' src='.\images\lightOff.png'>";
+			echo "<img STYLE='position:absolute; BOTTOM:30%; RIGHT:7%;' src='.\images\away.png'>";
+			echo "<img STYLE='position:absolute; BOTTOM:15%; RIGHT:7%;' src='.\images\Red.png'>";
 			echo "<img STYLE='position:absolute; TOP:", $y, "px; LEFT:", $x, "px;' src='.\images\bill.png'>";
 		} else {
 			echo "nothing";
